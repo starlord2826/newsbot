4030a1165f78f8cff2d6424c85bf29670f7c84c7
starlord28 <31995318+starlord28@users.noreply.github.com>
2018-07-30 07:59:27 +0530

    Add files via upload

--- newsbot.py
+++ newsbot.py
@@ -2,6 +2,9 @@ from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
 from watson_developer_cloud import ConversationV1
 import json
 import requests
+from gtts import gTTS
+import os
+
 
 context = None
 
@@ -22,53 +25,95 @@ def message(bot, update):
     print('Received an update')
     global context
 
-    conversation = ConversationV1(username='161a18e1-8ddc-404c-894b-26470e295d90',  # TODO
-                                  password='d07Jb1MQduZM',  # TODO
+    conversation = ConversationV1(username='b4e9efc3-5841-4415-843c-113f0674416b',  # TODO
+                                  password='lduCs2dmYAyQ',  # TODO
                                   version='2018-02-16')
 
     # get response from watson
     response = conversation.message(
-        workspace_id='cb90b423-b0c4-45b5-9ee2-002f8ebdd6de',  # TODO
+        workspace_id='96c8fe8b-43a7-4243-8285-ad738f1748d1',  # TODO
         input={'text': update.message.text},
         context=context)
     print(json.dumps(response, indent=2))
     
     context = response['context']
-    print(context)
+   
 
     # build response
     resp = ''
+    categ=''
     ret=''
     esp=''
+    res=''
     es=''
-    for i in response['intents']:
-        	es += i['intent']
-    if es!='Bot_Control_Approve_Response':
+    source=''
+    rett=''
+    
+    for text in response['intents']:
+        	es += text['intent']
+    if es!='Bot_Control_Approve_Response' and es!='final' and es!='abcd':
 	for text in response['output']['text']:
         	resp += text
     	update.message.reply_text(resp)
-    else: 
+    if es=='abcd':
+               
+       	       res=response['input']['text']
+               print(2)
+
+               n = requests.get('https://newsapi.org/v2/everything?q='+ res +'&sortBy=popularity&apiKey=85c3ead119524a58899bde57a9e38032')
+
+	       obj = n.json()
+	       re = int(obj['totalResults'])
+	       ret = ''
+	       ret += str(obj['articles'][1]['url'])
+	       print(ret)
+
+	       update.message.reply_text(ret)
+
+	
+    elif es!='final' and es=='Bot_Control_Approve_Response':
+	 
+	 
+	 #update.message.reply_text("http://127.0.0.1:5000/")
 	 for text in response['output']['text']:
         	resp += text
          count=response['context']['count']
 	 
          category=response['context']['category']
-         n=requests.get('https://newsapi.org/v2/top-headlines?country='+count+'&category='+category+'&apiKey=9ef6f45b13b94839b709d56d1b728ab6')
+         
+         n=requests.get('https://newsapi.org/v2/top-headlines?country='+count+'&category='+category+'&apiKey=0b2bb070c6074bcfa178b18b35a69ba9')
 	 obj=n.json()
-	 ret+=str(obj['articles'][10]['url'])
+	 #ret+=str(obj['articles'][1]['url'])
+	 print(7)
+	 for i in range(0,10):
+		ret+=str(i+1)+':  '+str(obj['articles'][i]['title'])+'\n'+'\n'
 	 update.message.reply_text(resp+'\n'+'\n'+ret)
-    
+    else:
+	 
+	 num=response['context']['number']
+	 count=response['context']['count']
+	 
+         category=response['context']['category']
+ 	 print(num)
+         n=requests.get('https://newsapi.org/v2/top-headlines?country='+count+'&category='+category+'&apiKey=0b2bb070c6074bcfa178b18b35a69ba9')
+	 obj=n.json()
+	 #source='we have got this news from '+str(obj['articles'][1323]['source']['name'])+'\n'+'\n'
+	 ret+=str(obj['articles'][num-1]['url'])
+	 update.message.reply_text(ret)
+	 rett+=str(obj['articles'][num-1]['title'])
+         tts=gTTS(text=rett,lang='en')
+	 tts.save("good.mp3")
+	 os.system("cvlc good.mp3")
+	 #update.message.reply_audio(audio='/home/sandesh/projects/project/newsbot/good.mp3')
+	 #bot.send_audio(chat_id=systbotbot,  audio=open('/home/sandesh/projects/project/newsbot/good.mp3', 'rb'))
 	
-
-
-
+	
+	
     
-    
-
 
 def main():
     # Create the Updater and pass it your bot's token.
-    updater = Updater('639931768:AAE4yJYO_HlMG7QL4RPwa0c4tScm3A-bQo4')  #TODO
+    updater = Updater('630960342:AAGOpUpEw10u9Vd6gWaVUx5c4Fuy1unUbOc')  #TODO
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
