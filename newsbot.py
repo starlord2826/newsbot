7c1b375e7fcd3372576ba5f7b6d3d69da152b5ad
starlord28 <31995318+starlord28@users.noreply.github.com>
2018-08-02 14:27:56 +0530

    Update newsbot.py

--- newsbot.py
+++ newsbot.py
@@ -131,7 +131,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, message))
 
     # Start the Bot
     updater.start_polling()
